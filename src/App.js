bd7186163a1b649e4f0f0dae5bbc05bc6ae7a0f6
Hardyguardy <fighter2007max@gmail.com>
2024-02-10 20:38:14 +0100

    feat: added a few movies/updated style

--- src/App.js
+++ src/App.js
@@ -14,13 +14,18 @@ function App() {
   return (
     <div className="App flex justify-center items-center min-h-screen bg-gray-100">
       <div className="text-center">
-        <h1 class="text-2xl font-bold text-centerr">Pick a random movie</h1>
-        <button
-          className="bg-blue-500 h-[50px] hover:bg-blue-700 text-white "
-          onClick={pickRandomMovie}
-        >
-          Pick a movie
-        </button>
+        <h1 class="text-3xl font-bold text-center p-[20px]">
+          Pick a random movie
+        </h1>
+        <div className="pb-[30px]">
+          <button
+            className="bg-blue-500 h-[50px] w-[150px] hover:bg-blue-700 text-white rounded-[12px]  "
+            onClick={pickRandomMovie}
+          >
+            Pick a movie
+          </button>
+        </div>
+
         {selectedMovie && (
           <Movie name={selectedMovie.name} poster={selectedMovie.poster} />
         )}
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,9 +1,9 @@
 function Movie({ name, poster }) {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded-[20px] overflow-hidden shadow-lg">
       <img className="w-full" src={poster} alt={`Poster of ${name}`} />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{name}</div>
+        <div className="font-bold text-2xl mb-2">{name}</div>
       </div>
     </div>
   );
